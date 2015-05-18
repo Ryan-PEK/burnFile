108607ebeb4f1b004c458c0ca87e44d6b485ef4d
云笈 <yunji.ll@alibaba-inc.com>
2015-05-18 15:41:50 +0800

    fix db bug

--- server.js
+++ server.js
@@ -7,15 +7,17 @@ var app = express();
 var done = false;
 
 var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
 var file = __dirname + "/" + "file.db";
 var exists = fs.existsSync(file);
 
 if (!exists) {
     console.log("Creating DB file.");
     fs.openSync(file, "w");
+    var db = new sqlite3.Database(file);
+    db.run("CREATE TABLE FILELIST (filename TEXT)");
+    db.close();
 }
-
-var sqlite3 = require("sqlite3").verbose();
 
 /*Configure the multer.*/
 
@@ -47,27 +49,28 @@ app.get('/', function(req, res) {
 app.post('/upload', function(req, res) {
     if (done === true) {
         console.log(req.files);
-        res.end("File uploaded.");
         var db = new sqlite3.Database(file);
         db.serialize(function() {
-            if (!exists) {
-                db.run("CREATE TABLE FILELIST (filename TEXT)");
-            }
-
             var stmt = db.prepare("INSERT INTO FILELIST VALUES (?)");
             stmt.run(req.files.file.name);
+            console.log(req.files.file.name);
             stmt.finalize();
         });
         db.close();
+        res.end("File uploaded.");
     }
 });
 
 app.get('/latest', function(req, res) {
     var db = new sqlite3.Database(file);
     db.serialize(function() {
-        db.each("SELECT rowid AS id, filename FROM FILELIST ORDER BY rowid desc LIMIT 1", function(err, row) {
-            console.log("Latest File: " + row.id + ": " + row.filename);
-            res.send('<img src="/uploads/'+ row.filename + '">');
+        db.get("SELECT rowid AS id, filename FROM FILELIST ORDER BY rowid desc LIMIT 1", function(err, row) {
+            if (row) {
+                console.log("Latest File: " + row.id + ": " + row.filename);
+                res.send('<img src="/uploads/' + row.filename + '">');
+            } else {
+                res.send('');
+            }
         });
     });
     db.close();
